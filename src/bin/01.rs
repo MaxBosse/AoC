336da244a47fb681190d8f36811356ad129b082a
Max Boße <git@bosse.io>
2023-12-02 21:29:22 +0100

    Done Day 2

---
+++ src/bin/01.rs
@@ -0,0 +1,102 @@
+use std::char::from_digit;
+
+advent_of_code::solution!(1);
+
+pub fn part_one(input: &str) -> Option<u32> {
+    let mut total:u32 = 0;
+
+    for line in input.lines() {
+        let mut first_digit = '0';
+        let mut second_digit = '0';
+
+        for c in line.chars() {
+            if c.is_numeric() {
+                first_digit = c;
+                break;
+            }
+        }
+
+        for c in line.chars().rev() {
+            if c.is_numeric() {
+                second_digit = c;
+                break;
+            }
+        }
+
+        let num: u32 = format!("{}{}", first_digit, second_digit).parse().unwrap();
+        total += num;
+    }
+
+    Some(total)
+}
+
+pub fn part_two(input: &str) -> Option<u32> {
+    let numbers = ["one", "two", "three", "four", "five", "six", "seven", "eight", "nine"];
+    let mut total:u32 = 0;
+
+    for line in input.lines() {
+        let mut first_digit = '0';
+        let mut second_digit = '0';
+
+
+        'outer: for (i, c) in line.chars().enumerate() {
+            if c.is_numeric() {
+                first_digit = c;
+                break;
+            }
+            for y in 0..numbers.len() {
+                let number = numbers[y];
+                if i+number.len() > line.len() {
+                    continue;
+                }
+
+                if line[i..(number.len()+i)].starts_with(number) {
+                    first_digit = from_digit((y+1).try_into().unwrap(), 10).unwrap();
+                    break 'outer;
+                }
+            }
+        }
+
+        'outer: for (i, c) in line.chars().rev().enumerate() {
+            let z = line.chars().count() - i - 1;
+            if c.is_numeric() {
+                second_digit = c;
+                break;
+            }
+            for y in 0..numbers.len() {
+                let number = numbers[y];
+                if z+number.len() > line.len() {
+                    continue;
+                }
+
+                if line[z..(number.len()+z)].starts_with(number) {
+                    second_digit = from_digit((y+1).try_into().unwrap(), 10).unwrap();
+                    break 'outer;
+                }
+            }
+        }
+
+        let num: u32 = format!("{}{}", first_digit, second_digit).parse().unwrap();
+        println!("{}", num);
+        total += num;
+    }
+
+    Some(total)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_part_one() {
+        let result = part_one(&advent_of_code::template::read_file("examples", DAY));
+        assert_eq!(result, Some(329));
+    }
+
+    #[test]
+    fn test_part_two() {
+        let result = part_two(&advent_of_code::template::read_file("examples", DAY));
+        assert_eq!(result, Some(281));
+    }
+}
---
+++ src/bin/02.rs
@@ -0,0 +1,136 @@
+use regex::Regex;
+
+advent_of_code::solution!(2);
+
+pub fn part_one(input: &str) -> Option<u32> {
+    let mut total: u32 = 0;
+
+    let max_red = 12;
+    let max_green = 13;
+    let max_blue = 14;
+
+    let re = Regex::new(r"^Game ([0-9]+): (.*)$").unwrap();
+
+    'outer: for line in input.lines() {
+        let Some((_full, [game_id, games])) = re.captures(line).map(|c| c.extract()) else { continue; };
+
+        let game_num: u32 = game_id.parse().unwrap();
+        let mut cur_group_num = 0;
+
+        for game in games.split(";") {
+            for group in game.split(",") {
+                for pair in group.split(" ") {
+                    if pair == "" {
+                        continue;
+                    }
+                    
+                    if cur_group_num == 0 {
+                        cur_group_num = pair.parse().unwrap();
+                        continue;
+                    }
+
+                    match pair {
+                        "green" => {
+                            if cur_group_num > max_green {
+                                continue 'outer;
+                            }
+                        },
+                        "red" => {
+                            if cur_group_num > max_red {
+                                continue 'outer;
+                            }
+                        },
+                        "blue" => {
+                            if cur_group_num > max_blue {
+                                continue 'outer;
+                            }
+                        }
+                        _ => println!("Unknown pair: {}", pair),
+                    }
+                }
+                cur_group_num = 0;
+            }
+        }
+
+        total += game_num;
+    }
+
+    Some(total)
+}
+
+pub fn part_two(input: &str) -> Option<u32> {
+    let mut total: u32 = 0;
+
+    let mut max_red: u32 = 0;
+    let mut max_green: u32 = 0;
+    let mut max_blue: u32 = 0;
+
+    let re = Regex::new(r"^Game ([0-9]+): (.*)$").unwrap();
+
+    for line in input.lines() {
+        let Some((_full, [game_id, games])) = re.captures(line).map(|c| c.extract()) else { continue; };
+
+        let game_num: u32 = game_id.parse().unwrap();
+        let mut cur_group_num = 0;
+
+        for game in games.split(";") {
+            for group in game.split(",") {
+                for pair in group.split(" ") {
+                    if pair == "" {
+                        continue;
+                    }
+                    
+                    if cur_group_num == 0 {
+                        cur_group_num = pair.parse().unwrap();
+                        continue;
+                    }
+
+                    match pair {
+                        "green" => {
+                            if cur_group_num > max_green {
+                                max_green = cur_group_num;
+                            }
+                        },
+                        "red" => {
+                            if cur_group_num > max_red {
+                                max_red = cur_group_num;
+                            }
+                        },
+                        "blue" => {
+                            if cur_group_num > max_blue {
+                                max_blue = cur_group_num;
+                            }
+                        }
+                        _ => println!("Unknown pair: {}", pair),
+                    }
+                }
+                cur_group_num = 0;
+            }
+        }
+
+        println!("Min Green: {}, Min Blue: {}, Min red: {}", max_green, max_blue, max_red);
+        total += max_blue * max_green * max_red;
+        max_red = 0;
+        max_green = 0;
+        max_blue = 0;
+    }
+
+    Some(total)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_part_one() {
+        let result = part_one(&advent_of_code::template::read_file("examples", DAY));
+        assert_eq!(result, Some(8));
+    }
+
+    #[test]
+    fn test_part_two() {
+        let result = part_two(&advent_of_code::template::read_file("examples", DAY));
+        assert_eq!(result, Some(2286));
+    }
+}
